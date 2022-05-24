453a1e5548df7367902cbbfb903dcd6c61c1f235
ShinjykuHanako <105779789+ShinjykuHanako@users.noreply.github.com>
2022-05-24 14:01:49 +0900

    sos

---
+++ lesson05/sample88.php
@@ -0,0 +1,36 @@
+<?php
+function uranai($age, $sex){
+
+    $amari = $age % 3;
+
+    if($sex = "woman"){
+        $amari++;
+    }
+
+    $kekka  = "";
+    if($amari == 0){
+        $kekka = "凶";
+    }elseif($amari == 1){
+        $kekka = "小吉";
+    }elseif($amari == 2){
+        $kekka = "中吉";
+    }else{
+        $kekka = "大吉";
+    }
+        return $kekka;
+}
+
+///////////////////////////////////////
+
+$nemrei = 28;
+$seibetu = "man";
+
+$watasi = uranai($nemrei, $seibetu);
+
+if($watasi != "凶"){
+    echo "<h1 style='color:red;'>{$watasi}</h1>";
+}else{
+    echo "<h1 style='color:blue;'>今日は占いはお休みみたい！！</h1>";
+}
+
+
