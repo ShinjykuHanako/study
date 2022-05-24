714ef567964d00397c942a8c08c6458a79e00cd6
ShinjykuHanako <105779789+ShinjykuHanako@users.noreply.github.com>
2022-05-24 15:24:47 +0900

    sos

--- lesson05/sample13.php
+++ lesson05/sample13.php
@@ -1,6 +1,7 @@
 <?php
 function plus(int $n1, int $n2) {
-  echo $n1 + $n2;
+//function plus($n1, $n2) {
+    echo $n1 + $n2;
 }
 
 plus(10, 5);
--- lesson05/sample14.php
+++ lesson05/sample14.php
@@ -2,7 +2,7 @@
 function plus(int $n1, int $n2): int {
   return $n1 + $n2;
 }
-function hello(): int {
+function hello(): String {
   return 'hello';
 }
 
