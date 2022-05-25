07b88d667ddca7ee831505473ee9028b13e7a707
ShinjykuHanako <105779789+ShinjykuHanako@users.noreply.github.com>
2022-05-25 14:40:43 +0900

    sos

--- lesson06/sample05.php
+++ lesson06/sample05.php
@@ -1,29 +1,4 @@
 <?php
-class Car {
-  public $name;
-  public $color;
-  public $speed;
-  public $fuel;
-
-  function __construct($name, $color, $speed, $fuel) {
-    $this->name = $name;
-    $this->color = $color;
-    $this->speed = $speed;
-    $this->fuel = $fuel;
-  }
-
-  function accellerator() {
-    $this->speed += 5;
-    $this->fuel -= 5;
-  }
-
-  function display() {
-    echo '車の名前は' . $this->name . 'です。', '<br />';
-    echo '車の色は' . $this->color . 'です。', '<br />';
-    echo '車の速度は' . $this->speed . 'kmです。', '<br />';
-    echo '車のガソリン量は' . $this->fuel . 'Lです。', '<br />';
-  }
-}
 
 $c1 = new Car('ヤリス', '赤', 0, 100);
 
